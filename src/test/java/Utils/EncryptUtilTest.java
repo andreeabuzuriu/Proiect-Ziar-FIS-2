9229aef4f209cddbacab59931174615d830da576
Andreea Buzuriu <andreea_buzuriu@yahoo.com>
2020-06-20 13:38:25 +0300

    Added encrypt test

---
+++ src/test/java/Utils/EncryptUtilTest.java
@@ -0,0 +1,19 @@
+package Utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+public class EncryptUtilTest extends ApplicationTest {
+
+    @Test
+    public void testEncrypt()
+    {
+        String password = "parola";
+        String encrypted = EncryptUtil.encrypt(password);
+        boolean areEquals=true;
+        if(!password.equals(encrypted)) //daca nu sunt egale, falses
+            areEquals=false;
+        Assert.assertEquals(false,areEquals);
+    }
+}
