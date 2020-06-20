3e7dc5b06d549fdffb83472dc15c6b146cbf61cf
Andreea Buzuriu <andreea_buzuriu@yahoo.com>
2020-06-20 13:44:26 +0300

    Added testReadUsersFile (or with null) - finished UserUtils testing

--- src/test/java/Utils/UserUtilsTest.java
+++ src/test/java/Utils/UserUtilsTest.java
@@ -1,9 +1,16 @@
 package Utils;
 
+import Models.UserModel;
 import Models.UserType;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.Assert;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
 
 public class UserUtilsTest extends ApplicationTest {
 
@@ -18,5 +25,21 @@ public class UserUtilsTest extends ApplicationTest {
         UserType userType = UserUtils.isUserValid("adi","parola");
         Assert.assertNotNull(userType);
     }
-    
+
+    @Test(expected = FileNotFoundException.class)
+    public void testReadUsersFileWrongFile() throws FileNotFoundException {
+        FileReader reader = new FileReader("FisierInexistent.json");
+    }
+
+
+    @Test
+    public void testReadUsersFile() throws FileNotFoundException {
+        Gson gson = new Gson();
+        FileReader reader = new FileReader("users.json");
+        ArrayList<UserType> users = gson.fromJson(reader, new TypeToken<ArrayList<UserModel>>() {
+        }.getType());
+        Assert.assertNotNull(users);
+    }
+
+
 }
